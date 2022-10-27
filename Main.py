6adadd7594ea8e1b208a05f055659079d547ad33
Linon0 <111737544+Linon0@users.noreply.github.com>
2022-10-27 18:37:40 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import csv
 from googletrans import Translator
 from pprint import pprint
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 
 class MyWidget(QMainWindow):
@@ -37,21 +38,21 @@ class MyWidget(QMainWindow):
                 for index, row in enumerate(reader):
                     if row[0] == self.country:
                         self.iso = row[4]
-                        self.iso = self.iso[-1:-3]
+                        self.iso = self.iso[-2:]
                         break
-
             r1 = requests.get(
-                f"http://api.openweathermap.org/geo/1.0/direct?q={self.city},{self.iso}&limit={1}&appid={self.open_weather_token}"
+                f"http://api.openweathermap.org/geo/1.0/direct?q={self.city},{self.country}&limit={1}&appid={self.open_weather_token}"
             )
             self.data1 = r1.json()
             self.data1 = self.data1[0]
             self.lat = self.data1['lat']
             self.lon = self.data1['lon']
-            r2 = requests.get(
-                f"https://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.open_weather_token}&units={'metric'}&lang={'ru'}"
+            response = requests.get(
+                f"https://yandex.ru/pogoda/?lat={self.lat}&lon={self.lon}"
             )
-            self.data = r2.json()
+            self.bs = BeautifulSoup(response.text, 'lxml')
             self.show_weather()
+
         except Exception:
             self.error = QMessageBox()
             self.error.setWindowTitle('Ошибка')
@@ -71,21 +72,25 @@ class MyWidget(QMainWindow):
                 self.get_weather()
 
     def show_weather(self):
-        self.time = datetime.today().strftime('%H:%M')
-        self.degrees = str(int(self.data['main']['temp'])) + '°'
-        self.feels_like = 'Ощущается как ' + str(int(self.data['main']['feels_like'])) + '°'
-        self.weather = self.data['weather'][0]['description']
-        self.wind = 'Ветер - ' + str(self.data['wind']['speed']) + ' м/с'
-        self.humidity = 'Влажность - ' + str(self.data['main']['humidity']) + '%'
-        self.sunrise = 'Рассвет - ' + str(datetime.fromtimestamp(self.data['sys']['sunrise']).strftime('%H:%M'))
-        self.sunset = 'Закат - ' + str(datetime.fromtimestamp(self.data['sys']['sunset']).strftime('%H:%M'))
+        self.city = self.bs.find('h1', class_='title title_level_1 header-title__title').text
+        self.time = self.bs.find('time', class_='time fact__time').text[7:12]
+        self.degrees = self.bs.find('span', class_='temp__value temp__value_with-unit').text
+        self.feels_like = 'Ощущается как ' + self.bs.find('span', class_='temp__value temp__value_with-unit').text
+        self.weather = self.bs.find('div', class_='link__condition day-anchor i-bem').text
+        self.wind = 'Ветер - ' + self.bs.find('div', class_='term term_orient_v fact__wind-speed').find('div', class_='term__value').text
+        self.humidity = 'Влажность - ' + self.bs.find('div', class_='term term_orient_v fact__humidity').text[:3]
+        self.sunrise = self.bs.find('div', class_='sun-card__sunrise-sunset-info sun-card__sunrise-sunset-info_value_rise-time').text[:6] + \
+                       ' - ' + self.bs.find('div', class_='sun-card__sunrise-sunset-info sun-card__sunrise-sunset-info_value_rise-time').text[6:]
+        self.sunset = self.bs.find('div', class_='sun-card__sunrise-sunset-info sun-card__sunrise-sunset-info_value_set-time').text[:5] + \
+                      ' - ' + self.bs.find('div', class_='sun-card__sunrise-sunset-info sun-card__sunrise-sunset-info_value_set-time').text[5:]
+        print(self.degrees)
 
-        text_date_time = self.city + '. Погода на ' + str(self.time)
+        text_date_time = self.city + '. Погода на ' + self.time
         self.label_date_time.setText(text_date_time)
         self.label_degrees.setText(self.degrees)
         self.label_weather.setText(self.weather)
         self.label_degrees_of_feeling.setText(self.feels_like)
-
+        print(text_date_time)
         self.label_wind.setText(self.wind)
         self.label_humidity.setText(self.humidity)
         self.label_sunrise.setText(self.sunrise)
